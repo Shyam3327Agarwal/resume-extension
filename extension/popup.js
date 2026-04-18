5803f78ed57c4a06650ff2ce45e5fc0ea6216d74
Shyam3327Agarwal <shyama@mindfiresolutions.com>
2026-04-18 17:02:18 +0530

    Standardized deployment structure: renamed backend to functions and updated firebase config

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://chome-extension-for-review.web.app/api';
 
 // UI Elements
 const loginView = document.getElementById('loginView');
